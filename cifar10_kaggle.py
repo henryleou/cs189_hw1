93fa019dfe4f907e2480247369f50bab790ed852
henryleou <henryleou927@gmail.com>
2019-01-30 05:02:29 -0800

    clean

--- cifar10_kaggle.py
+++ cifar10_kaggle.py
@@ -17,20 +17,12 @@ cifar10 = io.loadmat("data/cifar10_data.mat")
 num_samples = len(cifar10["training_data"])
 index_arr = np.random.choice(num_samples, num_samples, replace=False)
 
-all_train_labels = cifar10["training_labels"][index_arr][5000:]
-all_train_data = cifar10["training_data"][index_arr][5000:]
-all_val_labels = cifar10["training_labels"][index_arr][:5000]
-all_val_data = cifar10["training_data"][index_arr][:5000]
-
-#t_data = vstack([all_train_data, all_val_data])
-#t_labels = np.append(all_train_labels.ravel(), all_val_labels.ravel())
-
 test_data = cifar10["test_data"]
 train_data = cifar10["training_data"][index_arr]
 train_labels = cifar10["training_labels"][index_arr].ravel()
 print("num test data" + str(len(test_data)))
 
-num_train = 5000
+num_train = 20000
 t_data = train_data[:num_train]
 t_labels = train_labels[:num_train]
 val_data = train_data[num_train:num_train+1000]
